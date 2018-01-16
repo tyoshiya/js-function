24b207f05b5ad98239bb2e6aa15faa721b7bfc19
tyoshiya <tyoshiya.xeno.works@gmail.com>
2018-01-16 15:06:25 +0900

    getFirstKey

---
+++ function.js
@@ -0,0 +1,13 @@
+$(function(){
+//***********************************************
+//
+// get first key
+//
+//***********************************************
+    var getFirstKey = function(map){
+        for (var key in map) {
+            return key;
+        }
+        return null;
+    }
+});
