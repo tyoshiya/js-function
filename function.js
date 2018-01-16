fe09da0498b8c6cfdd07185bd0e9ceb1f015945f
tyoshiya <tyoshiya.xeno.works@gmail.com>
2018-01-16 15:54:45 +0900

    getRandom

--- function.js
+++ function.js
@@ -3,11 +3,36 @@ $(function(){
 //
 // get first key
 //
+// @map hash
+//
 //***********************************************
     var getFirstKey = function(map){
         for (var key in map) {
             return key;
         }
         return null;
+    }
+
+//***********************************************
+//
+// get random number
+//
+// @digit int
+// @decimal_digit int
+//
+//***********************************************
+    var getRandom = function(digit,decimal_digit){
+        if(typeof digit == "undefined"){
+            return 0;
+        }
+        var random = Math.random();
+        random *= Math.pow(10,digit);
+        if(typeof decimal_digit != "undefined"){
+            random *= Math.pow(10,decimal_digit);
+            random = Math.ceil(random);
+            return random / Math.pow(10,decimal_digit);
+        }else{
+            return Math.ceil(random);
+        }
     }
 });
