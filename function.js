f4fad4b8023f87179c5af9ac9d344e95ad013617
tyoshiya <tyoshiya.xeno.works@gmail.com>
2018-03-01 14:53:00 +0900

    getQueryString

--- function.js
+++ function.js
@@ -333,3 +333,19 @@ var getMargin = function(selector,direction){
         return margin;
     }
 }
+
+//***********************************************
+//
+// クエリストリング取得
+//
+//***********************************************
+var getQueryString = function(){
+    var arg = new Object;
+    var pair=location.search.substring(1).split('&');
+    for(var i=0;pair[i];i++) {
+        var kv = pair[i].split('=');
+        arg[kv[0]]=kv[1];
+    }
+
+    return arg;
+}
