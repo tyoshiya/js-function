bb7f6e786b8fffaf89581921fda539d9b8b842b2
tyoshiya <tyoshiya.xeno.works@gmail.com>
2018-03-01 11:06:54 +0900

    getNow

--- function.js
+++ function.js
@@ -1,24 +1,41 @@
 $(function(){
 //***********************************************
 //
-// get first key
-//
-// @map hash
+// 配列の一倍最初のキーを返す
 //
 //***********************************************
-    var getFirstKey = function(map){
-        for (var key in map) {
-            return key;
-        }
-        return null;
+var getFirstKey = function(arr_or_hash){
+    for (var key in arr_or_hash) {
+        return key;
     }
+    return null;
+}
 
 //***********************************************
 //
-// get random number
+// 配列の一番最後のキーを取得
 //
-// @digit int
-// @decimal_digit int
+//***********************************************
+var getLastKey = function(arr_or_hash){
+    var i = 1,
+        len = Object.keys(arr_or_hash).length
+        lastkey = "";
+
+    $.each(arr_or_hash,function(key,val){
+        if(i == len){
+            lastkey = key;
+        }
+        i++;
+    });
+    return lastkey;
+}
+
+//***********************************************
+//
+// ランダムな数値を返す 
+//
+// @digit 桁数
+// @decimal_digit 小数点以下桁数
 //
 //***********************************************
     var getRandom = function(digit,decimal_digit){
@@ -38,10 +55,10 @@ $(function(){
 
 //***********************************************
 //
-// get random number beteen min and max
+// 指定の数値の範囲内でランダムな桁数を返す
 //
-// @min int
-// @max int
+// @min int 最小値
+// @max int 最大値
 //
 //***********************************************
     var getRandomRange = function(min,max){
@@ -53,29 +70,29 @@ $(function(){
 
 //***********************************************
 //
-// sort array
+// 配列のソート
 //
 // @arr arr or hash
 // @key sortkey
 // @orderby "asc" or "desc"
 //
 //***********************************************
-    var sortArr = function(arr,key,orderby){
-        if(typeof arr == "undefined" || typeof key == "undefined" || typeof orderby == "undefined"){
-            return arr;
+    var sortarr_or_hash = function(arr_or_hash,key,orderby){
+        if(typeof arr_or_hash == "undefined" || typeof key == "undefined" || typeof orderby == "undefined"){
+            return arr_or_hash;
         }
         if(orderby != "asc" && orderby != "desc"){
-            return arr;
+            return arr_or_hash;
         }
         if(orderby == "asc"){
-            return arr.sort((a,b) => {
+            return arr_or_hash.sort((a,b) => {
                 if (Number(a[key]) < Number(b[key])) return -1;
                 if (Number(a[key]) > Number(b[key])) return 1;
                 return 0;
             });
         }
         if(orderby == "desc"){
-            return arr.sort((a,b) => {
+            return arr_or_hash.sort((a,b) => {
                 if (Number(a[key]) > Number(b[key])) return -1;
                 if (Number(a[key]) < Number(b[key])) return 1;
                 return 0;
@@ -86,10 +103,10 @@ $(function(){
 
 //***********************************************
 //
-// get height
+// 要素の高さを取得
 //
-// @obj obj height_object
-// @inout string "in" or "out"
+// @obj obj 高さを取得したいオブジェクト
+// @inout string "inner" or "outer"
 // @percentage int percentage
 //
 //***********************************************
@@ -117,38 +134,77 @@ var getHeight = function(obj,inout,percentage){
 
 //***********************************************
 //
-// get key arr
+// 配列のキーを全て取得する
 //
 // @hash hash hash
 //
 //***********************************************
-var getKeys = function(hash){
+var getKeys = function(arr_or_hash){
     var keys = [];
-    for(key in hash){
+    for(key in arr_or_hash){
         keys.push(key);
     }
 
-    console.log(keys);
     return keys;
 }
 
 //***********************************************
 //
-// get lastkey in hash
+// 指定した日付を指定したフォーマットで取得する
 //
-// @hash hash hash
+// @param format string フォーマット文字列
+//  @default Y-m-d
+// @param date string 指定する日付
+//  @default 本日の日付
 //
 //***********************************************
-var getLastKeyInHash = function(hash){
-    var i = 1,
-        len = Object.keys(hash).length
-        lastkey = "";
+var getNow = function(format,date){
+    if(typeof format == "undefined"){
+        format = "Y-m-d";
+    }
+    var split_format = [];
+    var format_len = format.length;
+    for(i = 0; i < format_len; i++){
+        split_format.push(format.slice(i,(i + 1))); 
+    }
 
-    $.each(hash,function(key,val){
-        if(i == len){
-            lastkey = key;
+    var new_date;
+    if(typeof date == "undefined"){
+        new_date = new Date(); 
+    }else{
+        new_date = new Date(date); 
+    }
+    var now = "";
+    split_format.forEach(function(val){
+        switch (val) {
+            case "Y":
+                now+= new_date.getFullYear();       
+                break;
+
+            case "m":
+                now+= ("00" + (new_date.getMonth() + 1)).slice(-2);
+                break;
+
+            case "d":
+                now+= ("00" + (new_date.getDate())).slice(-2);
+                break;
+
+            case "H":
+                now+= ("00" + (new_date.getHours())).slice(-2);
+                break;
+
+            case "i":
+                now+= ("00" + (new_date.getMinutes())).slice(-2);
+                break;
+
+            case "s":
+                now+= ("00" + (new_date.getSeconds())).slice(-2);
+                break;
+
+            default:
+                now+= val;
         }
-        i++;
     });
-    return lastkey;
+
+    return now;
 }
